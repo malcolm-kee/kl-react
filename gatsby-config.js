80ef71f07fa02339b24f45c7f436362d3b45c8a4
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-08-15 01:34:24 +0800

    add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
+  path: `.env.${process.env.NODE_ENV}`,
 });
 
 module.exports = {
   siteMetadata: {
     title: `KL React Meetup`,
     description: `Reacting in Kuala Lumpur, Malaysia`,
-    url: `https://kl-react.netlify.com`
+    url: `https://kl-react.netlify.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -16,11 +16,21 @@ module.exports = {
         key: process.env.MEETUP_API_KEY,
         groupUrlName: 'kl-react',
         status: 'upcoming,past',
-        desc: 'true'
-      }
+        desc: 'true',
+      },
     },
     `gatsby-theme-notes`,
     `gatsby-theme-conference`,
-    `gatsby-plugin-netlify`
-  ]
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `KL React`,
+        short_name: `KL React`,
+        background_color: `#33e`,
+        theme_color: `#33e`,
+        icon: `og-image/react_kl_icon.png`,
+      },
+    },
+    `gatsby-plugin-netlify`,
+  ],
 };
