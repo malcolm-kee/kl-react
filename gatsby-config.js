2b47c110440c6455b627e9b098e737eee5e149ca
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-08-24 23:40:23 +0800

    pull in data from github for vote topics page

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,17 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: 'GitHub',
+        fieldName: 'github',
+        url: 'https://api.github.com/graphql',
+        headers: {
+          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        },
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `image`,
---
+++ src/pages/vote-topics.js
@@ -0,0 +1,51 @@
+import { graphql } from 'gatsby';
+import React from 'react';
+import { Layout } from '../components/layout';
+import { Seo } from '../components/seo';
+
+const VoteTopics = ({ data, location }) => {
+  return (
+    <>
+      <Seo
+        title="Vote Talk Topics"
+        description="List of talk topics, voted by communities"
+        pathname={location.pathname}
+      />
+      <Layout>
+        <h1>TODO</h1>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </Layout>
+    </>
+  );
+};
+
+export default VoteTopics;
+
+export const pageQuery = graphql`
+  query {
+    github {
+      repository(name: "kl-react", owner: "malcolm-kee") {
+        issue(number: 5) {
+          comments(first: 100) {
+            nodes {
+              bodyHTML
+              author {
+                avatarUrl
+                url
+                ... on GitHub_User {
+                  name
+                }
+              }
+              reactionGroups {
+                content
+                users {
+                  totalCount
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
