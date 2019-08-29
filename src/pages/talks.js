3f6efaaf7b719dd425c5fda7dd58d5d66e1ba157
matt-writes-code <37174946+mayojich@users.noreply.github.com>
2019-08-29 12:13:11 +0800

    talks page

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -1,26 +1,68 @@
 // By Matt
 
+/** @jsx jsx */
+
+import { Container, jsx, Styled } from 'theme-ui';
 import { graphql } from 'gatsby';
 import React from 'react';
 import { Seo } from '../components/seo';
 import { Layout } from '../components/layout';
+import { Cast, Code, PlayCircle } from 'react-feather';
+import { IconLink } from '../components/icon-link';
+
+// Materials component
+function TalkMaterial({ type, url }) {
+  switch (type) {
+    case 'repo':
+      return (
+        <IconLink to={url} aria-label="See source code" title="See source code">
+          <Code />
+        </IconLink>
+      );
+
+    case 'demo':
+      return (
+        <IconLink to={url} aria-label="See live demo" title="See live demo">
+          <PlayCircle />
+        </IconLink>
+      );
+
+    case 'slide':
+      return (
+        <IconLink to={url} aria-label="See slides" title="See slides">
+          <Cast />
+        </IconLink>
+      );
+
+    default:
+      return null;
+  }
+}
 
 export default function TalkPage({ data }) {
   return (
     <>
       <Seo title="Talks - KL React" />
       <Layout>
-        <h1>Some talks</h1>
-        {data.allTalkYaml.edges.map(({ node }) => (
-          <div key={node.id}>
-            <h4>{node.title}</h4>
-            <p>{node.description}</p>
-            <p>{node.speaker.name}</p>
-            <ul>
-              <li>{node.materials[0].url}</li>
-            </ul>
-          </div>
-        ))}
+        <Container>
+          <Styled.h1 sx={{ mb: 5 }}>Talks</Styled.h1>
+          {data.allTalkYaml.edges.map(({ node }) => (
+            <div key={node.id}>
+              <Styled.h3>{node.title}</Styled.h3>
+              <Styled.p sx={{ mb: 2 }}>by {node.speaker.name}</Styled.p>
+              <Styled.p sx={{ mb: 2 }}>{node.description}</Styled.p>
+              <div sx={{ mb: 5 }}>
+                {node.materials.map(material => (
+                  <TalkMaterial
+                    type={material.type}
+                    url={material.url}
+                    key={node.id}
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
+        </Container>
       </Layout>
     </>
   );
