63241e547c9f1d16c1bf9809f4deb087bd190832
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-29 20:18:41 +0800

    tweak theme color

--- src/components/icon-link.js
+++ src/components/icon-link.js
@@ -11,12 +11,10 @@ export const IconLink = ({ to, href = to, ...props }) => (
       display: 'inline-block',
       p: 2,
       borderRadius: 8,
-      boxShadow: `0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)`,
       color: 'inherit',
       textDecoration: 'none',
       '&:hover': {
         color: 'primary',
-        boxShadow: `0px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)`,
       },
       '& > svg': {
         display: 'inline-block',
