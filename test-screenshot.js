f6e048dd6cc78e6c1fd471fde22e3626d53d63f2
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-08-05 18:38:35 +0800

    fix og image generation

--- test-screenshot.js
+++ test-screenshot.js
@@ -10,25 +10,55 @@ This file is used for development for the SEO image generation only.
     await screenshot(
       {
         nodes: [
+          // {
+          //   title: 'TypeScript for React Developer',
+          //   slug: 'c',
+          //   dateTime: 'Sat, 14 Dec 2019 9:30 AM',
+          //   venue: 'RiggitPlus',
+          //   instructors: [
+          //     {
+          //       image: 'https://github.com/malcolm-kee.png',
+          //       name: 'Malcolm Kee',
+          //     },
+          //   ],
+          //   icon: 'https://malcolm-misc.s3-ap-southeast-1.amazonaws.com/durian-react.png',
+          // },
           {
-            title: 'TypeScript for React Developer',
+            title: 'KL React Test Meetup 2022',
             slug: 'c',
-            dateTime: 'Sat, 14 Dec 2019 9:30 AM',
-            venue: 'RiggitPlus',
-            instructors: [
+            icon: 'https://malcolm-misc.s3-ap-southeast-1.amazonaws.com/durian-react.png',
+            dateTime: 'Sat, 14 August 2022 9:30 AM',
+            venue: 'My Home',
+            talks: [
               {
-                image: 'https://github.com/malcolm-kee.png',
-                name: 'Malcolm Kee',
+                title: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+                speakerName: 'Malcolm Kee',
+                speakerImages: ['https://github.com/malcolm-kee.png'],
+              },
+              {
+                title: 'Microfrontend with React',
+                speakerName: 'Malcolm Kee and Matthew Yong',
+                speakerImages: [
+                  'https://github.com/malcolm-kee.png',
+                  'https://github.com/matt-writes-code.png',
+                ],
+              },
+              {
+                title: 'Getting Started with Web Development',
+                speakerName: 'Awesome Speaker',
               },
             ],
-            icon:
-              'https://malcolm-misc.s3-ap-southeast-1.amazonaws.com/durian-react.png',
           },
         ],
         reporter: console,
+        skipIfExists: false,
       },
       {
-        template: path.resolve(__dirname, 'og-image-template', 'workshop.html'),
+        template: path.resolve(
+          __dirname,
+          'og-image-template',
+          'meetup-compact.html'
+        ),
       }
     );
   } catch (e) {
